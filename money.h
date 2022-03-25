15a81e6612ff75c2ac11885991825bafdc2ad970
1102-Hodges-Tiffani <thodges@nevada.unr.edu>
2022-03-25 10:27:13 -0700

    relational operators

--- money.h
+++ money.h
@@ -17,6 +17,41 @@ public:
 
 	Money(); //default constructor
 	Money(int dollars, int cents); //constructor that takes dollars and cents
+
+	//overloads of relational operators
+	bool operator<(const Money &rhs) const {
+		return all_cents < rhs.all_cents;
+	}
+	bool operator>(const Money &rhs) const {
+		return all_cents > rhs.all_cents;
+	}
+	bool operator<-(const Money &rhs) const {
+		return all_cents <- rhs.all_cents;
+	}
+	bool operator>=(const Money &rhs) const {
+		return all_cents >= rhs.all_cents;
+	}
+	bool operator!=(const Money &rhs) const {
+		return all_cents != rhs.all_cents;
+	}
+	bool operator==(const Money &rhs) const {
+		return all_cents == rhs.all_cents;
+	}
+
+	//overloads of math operators
+	bool operator+(const Money &rhs) const {
+		return all_cents + rhs.all_cents;
+	}
+	bool operator-(const Money &rhs) const {
+		return all_cents - rhs.all_cents;
+	}
+	
+
+	//overload of <<
+	friend std::ostream &operator<<(std::ostream &os, const Money &money){
+		os << "$" << money.all_cents * 0.01;
+		return os;
+	}
    
 
 
